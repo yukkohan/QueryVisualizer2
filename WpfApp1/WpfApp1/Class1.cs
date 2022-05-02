ba4b0918e3e5aacc054fc3c59ec40f957f0b3e6e
yukkohan <y-ohara@mba.nifty.com>
2022-05-02 14:01:33 +0900

    modify

---
+++ WpfApp1/WpfApp1/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Class1
+    {
+        private List<String> myList = new List<String>();
+        private String myTxt;
+
+        public Class1(String txt)
+        {
+            //大文字にして格納
+            myTxt = txt.ToUpper();
+
+            separateTxt();
+
+        }
+
+        private void separateTxt()
+        {
+            //正規表現
+            String myRegEx = "SELECT.+?FROM";
+
+            //正規表現サーチ
+            foreach (var txt in Regex.Matches(myTxt, myRegEx))
+            {
+                myList.Add(txt.ToString());
+            }
+
+        }
+
+        public String getPartTxt(int x)
+        {
+            try { 
+                return myList[x];
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+
+
+
+    }
+}
--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -27,9 +29,31 @@ namespace WpfApp1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            Class1 myClass = new Class1(this.txtBox.Text);
+
+            this.txtResult.Text = myClass.getPartTxt(0);
+
+
+            //var txt = Regex.Matches(txtBox.Text ,txtReg.Text);
+
+            //if (txt.Count > 0) {
+
+            //    List<String> ary = new List<string>();
+
+            //    foreach (var tx in txt)
+            //    {
+            //        ary.Add(tx.ToString());
+            //    }
+
+            //    //txtResult.Text = ary[0] + ary[1] ;
+
+
+            }
+
+
+
 
 
 
         }
     }
-}
